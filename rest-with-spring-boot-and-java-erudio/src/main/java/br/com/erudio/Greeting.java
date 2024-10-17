cacd489e84c83197544a59396822de4195de18b0
vitor ozorio <vitormanoelbasilio357@gmail.com>
2024-10-16 21:45:21 -0300

    Frist steps in java with Spring Boot 3

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,20 @@
+package br.com.erudio;
+
+public class Greeting {
+
+    private final long id;
+    public final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}

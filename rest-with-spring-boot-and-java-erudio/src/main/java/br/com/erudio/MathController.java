d8b42ceda333657e0c28409ad5b088b1402d8b4b
vitor ozorio <vitormanoelbasilio357@gmail.com>
2024-12-03 19:49:45 -0300

    61. 0604 Definindo os Métodos de Validação e Executando a Aplicação

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/MathController.java
@@ -13,9 +13,28 @@ public class MathController {
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(
             @PathVariable (value = "numberOne") String numberOne,
-            @PathVariable (value = "numberTwo") String numberTWO
-            ) {
-        return 1D;
+            @PathVariable (value = "numberTwo") String numberTwo
+            ) throws Exception {
+        if  (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return covertToDouble(numberOne) + covertToDouble(numberTwo);
     }
 
-}
+    public static Double covertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        // BR 10,25 US 10.25
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+    }
+
+
